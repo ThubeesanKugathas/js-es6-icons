d61f79c83228ecf0b3cf7bca38d79a5bc0f32e91
ThubeesanKugathas <Thubeesan@outlook.com>
2022-01-12 15:37:32 +0100

    filter by selected option /js

--- js/script.js
+++ js/script.js
@@ -115,19 +115,61 @@ const boxIconList = [
 
 const boxHtml = document.getElementById('box-icon');
 
+// createIcon(boxHtml, boxIconList);
+
+// function createIcon(box, array) {
+
+// 	array.forEach((icon) => {
+// 		box.innerHTML += 
+// 		`
+// 			<div class="col-2 ${icon.type}">
+// 				<i class="${icon.family} ${icon.prefix}${icon.name} ms_${icon.color}"></i>
+// 				<p>${icon.name.toUpperCase()}</p>
+// 			</div>
+// 		`
+// 	});
+// }
+
 boxIconList.forEach((icon) => {
-    boxHtml.innerHTML += 
-    `
-        <div class="col-2">
-            <i class="${icon.family} ${icon.prefix}${icon.name} ms_${icon.color}"></i>
-            <p>${icon.name.toUpperCase()}</p>
-        </div>
-    `
+	boxHtml.innerHTML += 
+	`
+		<div class="col-2 ${icon.type}">
+			<i class="${icon.family} ${icon.prefix}${icon.name} ms_${icon.color}"></i>
+			<p>${icon.name.toUpperCase()}</p>
+		</div>
+	`
 });
+
 
 const select = document.getElementById('select-filter');
 
 select.addEventListener('change', function () {
-	console.log(this.value);
-})
+	if (select.value == 2) {
+		const AnimalList = boxIconList.filter((attribute) => {
+			return attribute.type == "animal";
+		});
+		console.log(AnimalList);
+	} 
+	else if (select.value == 3) {
+		const VegetableList = boxIconList.filter((attribute) => {
+			return attribute.type == "vegetable";
+		});
+		console.log(VegetableList);
+	} 
+	else if (select.value == 4) {
+		const UserList = boxIconList.filter((attribute) => {
+			return attribute.type == "user";
+		});
+		console.log(UserList);
+	} 
+	else {
+		console.log(boxIconList);
+	}
+});
+
+
+
+
+
+
 
